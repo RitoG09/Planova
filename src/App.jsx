9157a1f45677ff7e83861f3203eb451110541243
Ritabrata Ghosh <76sonali40@gmail.com>
2025-01-19 05:32:40 +0530

    new trip page updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,4 @@
-import { useState } from "react";
 import "./App.css";
-import { Button } from "./components/ui/button";
 import Hero from "./components/custom/Hero";
 import Footer from "./components/custom/Footer";
 import Testimonials from "./components/custom/Testimonials";
---
+++ src/constants/options.jsx
@@ -0,0 +1,53 @@
+export const selectTravelesList = [
+  {
+    id: 1,
+    title: "Solo",
+    desc: "A sole travels in exploration",
+    icon: "🚵",
+    people: "1",
+  },
+  {
+    id: 2,
+    title: "Couple",
+    desc: "Two traveles in tandem",
+    icon: "🧑‍🤝‍🧑",
+    people: "2 People",
+  },
+  {
+    id: 3,
+    title: "Family",
+    desc: "A gorup of fun loving people",
+    icon: "👨‍👩‍👧‍👦",
+    people: "3 to 5 People",
+  },
+  {
+    id: 4,
+    title: "Friends",
+    desc: "A bunch of thrill-seekers",
+    icon: "🚌",
+    people: "5 to 10 People",
+  },
+];
+
+export const selectBudgetOption = [
+  {
+    id: 1,
+    title: "Cheap",
+    desc: "Stay concious about expenditure",
+    icon: "$",
+  },
+  {
+    id: 2,
+    title: "Moderate",
+    desc: "Average expenditure",
+    icon: "$$",
+  },
+  {
+    id: 3,
+    title: "Luxury",
+    desc: "Don't worry about cost",
+    icon: "$$$",
+  },
+];
+
+//export const AI_PROMPT = "Generate Travel Plan for location: {location}";
