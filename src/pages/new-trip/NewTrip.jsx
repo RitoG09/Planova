40688facc446764dbd75e7faf723235ed28729b0
Ritabrata Ghosh <76sonali40@gmail.com>
2025-01-19 05:32:40 +0530

    new trip page updated

--- src/pages/new-trip/NewTrip.jsx
+++ src/pages/new-trip/NewTrip.jsx
@@ -1,9 +1,35 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import GooglePlacesAutocomplete from "react-google-places-autocomplete";
-import { useSearchParams } from "react-router-dom";
+import {
+  selectBudgetOption,
+  selectTravelesList,
+} from "../../constants/options";
+import { Button } from "../../components/ui/button";
 
 function NewTrip() {
   const [place, setPlace] = useState();
+  const [formData, setFormData] = useState([]);
+
+  const handleInputChange = (name, value) => {
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
+
+  // useEffect(() => {
+  //   console.log(formData);
+  // }, [formData]);
+
+  const onGenerateTrip = () => {
+    if (formData?.noOfDays > 10) {
+      console.log("maximum 10 days trip can be planned");
+
+      return;
+    }
+    console.log(formData);
+  };
+
   return (
     <div className="sm:px-10 md:px-32 lg:px-56 xl:px-72 px-5 mt-10">
       <h2 className="text-4xl text-center font-extrabold">
@@ -14,9 +40,9 @@ function NewTrip() {
         us craft the perfect adventure for you."
       </p>
 
-      <div>
-        <div className="mt-20">
-          <h2 className="text-xl my-3 font-medium">
+      <div className="mt-20 flex flex-col gap-10">
+        <div>
+          <h2 className="text-2xl my-3 font-medium">
             What is your destination of choice?
           </h2>
           <GooglePlacesAutocomplete
@@ -25,11 +51,71 @@ function NewTrip() {
               place,
               onChange: (value) => {
                 setPlace(value);
-                console.log(value);
+                handleInputChange("location", value);
               },
             }}
           />
         </div>
+
+        <div>
+          <h2 className="text-2xl my-3 font-medium">
+            Select the duration of your days:
+          </h2>
+          <input
+            type="number"
+            className="border-2 rounded-sm px-5 py-1"
+            max={14}
+            min={2}
+            onChange={(e) => handleInputChange("noOfDays", e.target.value)}
+          />
+        </div>
+
+        <div>
+          <h2 className="text-2xl my-3 font-medium">What is your Budget?</h2>
+          <div className="grid grid-cols-3 gap-5">
+            {selectBudgetOption.map((item, index) => (
+              <div
+                key={index}
+                onClick={() => handleInputChange("budget", item.title)}
+                className={`p-4 border rounded-lg hover:shadow-lg 
+                cursor-pointer ${
+                  formData.budget == item.title && "shadow-lg border-yellow-600"
+                } `}
+              >
+                <h2 className="text-2xl">{item.icon}</h2>
+                <h2 className="text-2xl font-bold">{item.title}</h2>
+                <h2 className="text-lg text-gray-500">{item.desc}</h2>
+              </div>
+            ))}
+          </div>
+        </div>
+
+        <div>
+          <h2 className="text-2xl my-3 font-medium">
+            Who do you plan to travel with?
+          </h2>
+          <div className="grid grid-cols-3 gap-5">
+            {selectTravelesList.map((item, index) => (
+              <div
+                key={index}
+                onClick={() => handleInputChange("people", item.people)}
+                className={`p-4 border rounded-lg hover:shadow-lg cursor-pointer  ${
+                  formData.people == item.people &&
+                  "shadow-lg border-yellow-600"
+                }`}
+              >
+                <h2 className="text-2xl">{item.icon}</h2>
+                <h2 className="text-2xl font-bold">{item.title}</h2>
+                <h2 className="text-lg text-gray-500">{item.desc}</h2>
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+      <div className="my-20  justify-end flex">
+        <Button variant="destructive" onClick={onGenerateTrip}>
+          Generate Trip
+        </Button>
       </div>
     </div>
   );
