36173680260f99e969833a13965d20bcc5a2f013
Ritabrata Ghosh <76sonali40@gmail.com>
2025-02-05 02:01:21 +0530

    localhost update

--- api/server.js
+++ api/server.js
@@ -19,7 +19,7 @@ const connect = async () => {
   }
 };
 
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(cors({ origin: "http://localhost:5174", credentials: true }));
 app.use(express.json());
 app.use(cookieParser());
 
