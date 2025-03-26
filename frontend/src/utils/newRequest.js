9904c78c40171d1d13b9617029b962f49657eee3
Ritabrata Ghosh <76sonali40@gmail.com>
2025-03-27 03:13:29 +0530

    url change

--- frontend/src/utils/newRequest.js
+++ frontend/src/utils/newRequest.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const newRequest = axios.create({
-  baseURL: "http://localhost:5050/api/v1",
+  baseURL: "https://planova-web-backend.onrender.com/api/v1",
   withCredentials: true,
 });
 
