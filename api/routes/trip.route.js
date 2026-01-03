de2c3e36535129c32d811721c0a9c405ec9c8c21
Ritabrata Ghosh <76sonali40@gmail.com>
2026-01-03 13:45:34 +0530

    prod fix:04

--- api/routes/trip.route.js
+++ api/routes/trip.route.js
@@ -5,6 +5,7 @@ import { verifyToken } from "../middleware/jwt.js";
 const router = express.Router();
 
 router.post("/savedtrip",verifyToken, savedTrip);
-router.get("/history", verifyToken, getTripHistory);
+// router.get("/history", verifyToken, getTripHistory);
+router.get("/:id", verifyToken, getTripById); 
 
 export default router;
